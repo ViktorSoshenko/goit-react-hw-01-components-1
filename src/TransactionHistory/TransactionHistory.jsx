c0b063ec2c71b6043ba7cb46d274393d167d376d
ViktorSoshenko <viktors103@gmail.com>
2023-05-22 16:10:53 +0300

    add

---
+++ src/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import transactions from "./transactions.json";
+console.log(transactions);
+
+function Transaction(props) {
+  return transactions.map((transaction) => (
+    <tr key={transaction.id}>
+      <td>{transaction.type}</td>
+      <td>{transaction.amount}</td>
+      <td>{transaction.currency}</td>
+    </tr>
+  ));
+}
+
+function TransactionHistory(props) {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <Transaction />
+      </tbody>
+    </table>
+  );
+}
+export default TransactionHistory;
---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Profile from "../Profile/Profile";
+import Statistics from "../Statistics/Statistics";
+import FriendList from "../FriendList/FriendList";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+import data from "../Statistics/data.json";
+import friends from "../FriendList/friends.json";
+import transactions from "../TransactionHistory/transactions.json";
+const title = "Upload stats";
+const stats = data;
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "flex-start",
+        flexDirection: "column",
+        alignItems: "center",
+        fontSize: 40,
+        backgroundColor: "#e0e0e0",
+      }}
+    >
+      <Profile />
+      <Statistics title={title} stats={stats} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
+    </div>
+  );
+};
