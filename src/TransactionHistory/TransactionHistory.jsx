0b3910f2307091cd3bdb2b8b5faf0deddd33070e
ViktorSoshenko <viktors103@gmail.com>
2023-05-22 20:55:39 +0300

    Update TransactionHistory.jsx

--- src/TransactionHistory/TransactionHistory.jsx
+++ src/TransactionHistory/TransactionHistory.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import transactions from './transactions.json';
-import thmc from './TransactionHistory.module.css';
+
 console.log(transactions);
 
 function Transaction(props) {
